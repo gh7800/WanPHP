49b6dc7dbb101bb93d7e6f5a2a598105199ffa60
wangshuai <bjwangs@163.com>
2021-05-27 14:30:00 +0800

    ..

---
+++ Form/Form1.php
@@ -0,0 +1,5 @@
+<?php
+
+echo $_POST["name"];
+echo "\r\n";
+echo $_POST["age"];
--- app.php
+++ app.php
@@ -5,9 +5,8 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo $_POST["name"];
-echo "\r\n";
-echo $_POST["age"];
+
+echo phpinfo()
 ?>
 
 
